e0a4976945e455c8b5ab2f3319e033685a6a26ac
AlexArchibeque <wonkman33@gmail.com>
2020-10-24 15:46:54 -0700

    Starting twitter

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ const port = process.env.PORT || 5000;
 const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("hello asdasdasdasdasd world!"));
+app.get("/", (req, res) =>{
+
+  res.send("hello asdasdasdasdasd world!")
+});
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// routes
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
